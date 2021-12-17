e66ca7093cf53f428a69b8b0cf375decb7d0cf96
Jaison Jose <jaisonj1414@gmail.com>
2021-12-16 20:21:09 -0500

    API modified

--- app.py
+++ app.py
@@ -112,46 +112,7 @@ def column_chart():
     return render_template('charts.html',data=data_to_plot)
 
 
-    # page_connect = urlopen("https://understat.com/league/EPL")
-    # page_html = BeautifulSoup(page_connect, "html.parser")
-    # json_raw_string=str(page_html.findAll(name="script")[3])
-    # start_index = json_raw_string.index("\\")
-    # stop_index = json_raw_string.index("');")
-    # json_data=json_raw_string[start_index:stop_index]
-    # json_data=json_data.encode("utf8").decode("unicode_escape")
-    # data=json.loads(json_data)
-    # for record in data:
-    #     db.players_collection.insert_one(record)
-    client =MongoClient("mongodb+srv://JaisonJose:Password@cluster0.ood9a.mongodb.net/player_stats?ssl=true&ssl_cert_reqs=CERT_NONE")
-    db = client.get_database('player_stats_db')
-    records=db.players_collection
-    X=[]
-    Y1=[]
-    Y2=[]
-    Y3=[]
-    Y4=[]
-    Y5=[]
-    Y6=[]
-    for item in db.players_collection.find().limit(10):
-        X.append(item['player_name'])
-        Y1.append(item['goals'])
-        Y2.append(item['shots'])
-        Y3.append(item['assists'])
-        Y4.append(item['key_passes'])
-        Y5.append(item['yellow_cards'])
-        Y6.append(item['red_cards'])
-
-
-    data_to_plot=db.players_collection.find().limit(10)
-    string_data=str(data_to_plot)
-    print(string_data,file=sys.stderr)
-
-    # data={'Currency':'Price','BTC':48763.00,'ETH':4018.77,'BNB':532.46,'BCH':444.22}
-    return render_template('chart.html',data=data_to_plot)
-    # return (data_to_plot)
-
-@app.route('/allplayersdata',methods=['GET'])
-@app.route('/schedule')
+@app.route('/refreshstandings',methods=['GET'])
 def schedulejob():
     client =MongoClient("mongodb+srv://JaisonJose:Password@cluster0.ood9a.mongodb.net/player_stats?ssl=true&ssl_cert_reqs=CERT_NONE")
     db = client.get_database('player_stats_db')
@@ -171,7 +132,6 @@ def schedulejob():
     for record in data:
         db.players_collection.insert_one(record)
     
-    # data={'Currency':'Price','BTC':48763.00,'ETH':4018.77,'BNB':532.46,'BCH':444.22}
     return ('<h1>Data Entered in DB</h1>')
 
 if __name__=="__main__":
