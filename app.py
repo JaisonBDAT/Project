c673b6406c04c75d038f1524a7b3a448bd8b56d6
Jaison Jose <jaisonj1414@gmail.com>
2021-12-16 19:56:15 -0500

    images edited

--- app.py
+++ app.py
@@ -15,9 +15,40 @@ def index():
     return render_template('index.html')
 
     #return ('Hello World23')
-@app.route('/displaygraph')
-def displaygraph():
-    return render_template('chart.html')
+# @app.route('/displaygraph')
+# def displaygraph():
+#     return render_template('chart.html')
+
+
+@app.route('/allplayersdata',methods=['GET'])
+def totaldata():
+    client =MongoClient("mongodb+srv://JaisonJose:Password@cluster0.ood9a.mongodb.net/player_stats?ssl=true&ssl_cert_reqs=CERT_NONE")
+    db = client.get_database('player_stats_db')
+    records=db.players_collection
+    X=[]
+    Y1=[]
+    Y2=[]
+    Y3=[]
+    Y4=[]
+    Y5=[]
+    Y6=[]
+    list=[]
+    for item in db.players_collection.find():
+        list.append({'Name' : item['player_name'], 'Goal' : int(item['goals'])})
+
+    # for item in db.players_collection.find().limit(10):
+    #     X.append(item['player_name'])
+    #     Y1.append(item['goals'])
+    #     Y2.append(item['shots'])
+    #     Y3.append(item['assists'])
+    #     Y4.append(item['key_passes'])
+    #     Y5.append(item['yellow_cards'])
+    #     Y6.append(item['red_cards'])
+    data_to_plot=db.players_collection.find()
+    string_data=str(data_to_plot)
+    print(string_data,file=sys.stderr)
+    #return render_template('chart.html',data=data_to_plot)
+    return jsonify(list) 
 
 @app.route('/data')
 def data():
@@ -43,9 +74,9 @@ def data():
     #     Y4.append(item['key_passes'])
     #     Y5.append(item['yellow_cards'])
     #     Y6.append(item['red_cards'])
-    data_to_plot=db.players_collection.find().limit(10)
-    string_data=str(data_to_plot)
-    print(string_data,file=sys.stderr)
+    # data_to_plot=db.players_collection.find().limit(10)
+    # string_data=str(data_to_plot)
+    # print(string_data,file=sys.stderr)
     #return render_template('chart.html',data=data_to_plot)
     return jsonify(list)   
 
@@ -119,6 +150,7 @@ def column_chart():
     return render_template('chart.html',data=data_to_plot)
     # return (data_to_plot)
 
+@app.route('/allplayersdata',methods=['GET'])
 @app.route('/schedule')
 def schedulejob():
     client =MongoClient("mongodb+srv://JaisonJose:Password@cluster0.ood9a.mongodb.net/player_stats?ssl=true&ssl_cert_reqs=CERT_NONE")
